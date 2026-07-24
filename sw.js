048716fc80a4a04ecf50bf7ed0c162ca70900222
evvv amine <aminemouden210@gmail.com>
2026-07-24 02:01:37 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,17 @@
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11380569
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+const CACHE = "monetag-sw";
+const ASSETS = ["/"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  if (e.request.url.includes("monetag") || e.request.url.includes("pro-sterra") || e.request.url.includes("dolohen")) {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  }
+});
